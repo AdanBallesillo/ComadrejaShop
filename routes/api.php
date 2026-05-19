20304fa3c1ce84bde619e58deb5b4934311d6921
adan ballesillo velazquez <adan_@BALLESILLO1.COMADREJA2.local>
2026-05-19 09:12:00 -0600

    Cambios en API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Product;
+use App\Models\Category;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/products', function () {
-    return Product::all();
+    return Product::with('category', 'user')->get();
+});
+
+Route::get('/products/{id}', function ($id) {
+    return Product::with('category', 'user')->findOrFail($id);
+});
+
+Route::get('/categories', function () {
+    return Category::all();
 });
